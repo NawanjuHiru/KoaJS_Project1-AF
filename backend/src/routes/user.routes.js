7ab3ece15b99b2d92eee552bcd094481e2afa45b
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-27 01:22:02 +0530

    router implemented

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,10 @@
+const Router = require("@koa/router");
+const { saveUser } = require("../api/user.api");
+
+const router = new Router({
+    prefix:"/user",
+});
+
+router.post("/",saveUser);
+
+module.exports = router;
