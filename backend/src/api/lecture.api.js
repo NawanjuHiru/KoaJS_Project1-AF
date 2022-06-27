28bd8a8316760828a22a21bf1f78ccc9c013ce92
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-27 15:53:08 +0530

    lecture service implemented

---
+++ backend/src/api/lecture.api.js
@@ -0,0 +1,39 @@
+const Lecture = require("../models/lecture.model");
+const mongoose = require("mongoose");
+
+const saveLecture = async (ctx) => {
+    try {
+        const {
+            fullName,
+            email,
+            mobileNumber,
+            address,
+            isLecture,
+            faculty
+        } = ctx.request.body;
+
+        const lecture = await Lecture.create({
+            fullName : fullName,
+            email : email,
+            mobileNumber : mobileNumber,
+            address : address,
+            isLecture : isLecture,
+            faculty : faculty
+        });
+
+        return (ctx.body =
+            {
+                isSuccess : true,
+                message : "Lecture has been saved successfully."
+            });
+
+    } catch (error) {
+        return (ctx.body =
+            {
+                isSuccess : false,
+                message : "Error has been occured. Please try again."
+            });
+    }
+}
+
+module.exports = { saveLecture }
