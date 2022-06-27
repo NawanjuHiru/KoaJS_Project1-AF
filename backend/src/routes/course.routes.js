1270eb50be9ea62ae939f9ac61759c5bbf6bf31a
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-27 16:12:56 +0530

    course save  function implemented

---
+++ backend/src/routes/course.routes.js
@@ -0,0 +1,10 @@
+const Router = require("@koa/router");
+const { saveCourse } = require("../api/course.api");
+
+const router = new Router({
+    prefix:"/course"
+});
+
+router.post("/",saveCourse);
+
+module.exports = Router;
