ac7a58bfd894a1ea533364aa767fb5ce2fc85c70
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-28 13:11:53 +0530

    student and user services created

--- fontend/src/services/user/userService.js
+++ fontend/src/services/user/userService.js
@@ -1,2 +1,19 @@
 import axios from "axios";
-import environment from "../../"
+import environment from "../../config.dev";
+
+class UserService {
+    getUserDetails(userFilterModel) {
+        console.log(userFilterModel);
+        return axios.post(`${environment.apiUrl}user/getAll`, userFilterModel);
+    }
+
+    updateUser(userModel) {
+        return axios.put(`${environment.apiUrl}user/`, userModel);
+    }
+
+    deleteUser(id) {
+        return axios.delete(`${environment.apiUrl}user/` + id);
+    }
+}
+
+export default new UserService();
