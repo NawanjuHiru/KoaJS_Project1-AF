7bd62cf4454232c86ee7fd8e52fd9e88564cb848
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-06-28 12:40:22 +0530

    app.js file created

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,22 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import UpdateStudent from "./components/UpdateStudent";
-import AddStudent from "./components/AddStudent";
-import StudentList from "./pages/StudentList";
-import ViewStudent from "./components/ViewStudent";
+import "./App.css";
+import UserList from "./pages/user/user-list";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
   <div className="App">
-  <Router> 
-    <Switch>
-      <Route path="/" exact component={StudentList} />
-      <Route path="/add" exact component={AddStudent} />
-      <Route path="/update/:id" exact component={UpdateStudent} />
-      <Route path="/view/:id" exact component={ViewStudent} />
-    </Switch>
-  </Router>
-  <br/> <br/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element = {<UserList></UserList>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </div>
   );
 }
